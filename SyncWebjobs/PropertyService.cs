6fa776407e4ead6ad11a65079853cef57847043d
Tripti Tripathi <tripti.tripathi@mrisoftware.com>
2023-12-14 18:33:12 +0530

    added azure function skeleton and some change in console app

--- SyncWebjobs/PropertyService.cs
+++ SyncWebjobs/PropertyService.cs
@@ -16,8 +16,8 @@ namespace SyncWebjobs
 
         public IEnumerable<string> GetAllPropertyIds()
         {
-            IEnumerable<string> ids = new List<string>();
-            string connectionString = "";
+            List<string> ids = new List<string>();
+            string connectionString = "Server=sql-eastus-pna-qa.database.windows.net;Database=sqldb-eastus-pna-portal-qa;User Id=zLZ3tOGLxjk3Wx69RvyY;Password='ETeIP2N5XhOPYL71R2mj';MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30; ";
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
@@ -26,19 +26,21 @@ namespace SyncWebjobs
                 string query = "SELECT [PropertyId] FROM [dbo].[PropertySync]";
                 using (SqlCommand command = new SqlCommand(query, connection))
                 {
-                    // Execute the query
+                    //Execute the query
                     using (SqlDataReader reader = command.ExecuteReader())
                     {
                         while (reader.Read())
                         {
                             // Access data from the reader
-                            ids.Append(reader.GetString(0));
+                            //ids.Append(reader.GetString(0));
+                            string propertyId = reader.GetString(0);
+                            ids.Add(propertyId);
                         }
                     }
                 }
             }
             return ids;
         }
-        
+
     }
 }
