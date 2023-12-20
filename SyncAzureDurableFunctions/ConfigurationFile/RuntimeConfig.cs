8ed55a0634c38584b7b5e3f865717907b156742b
AHosamani <127177095+AHosamani@users.noreply.github.com>
2023-12-20 11:47:53 +0530

    Configuration in azure functions project
    Implementation of configuration in azure functions project.

---
+++ SyncAzureDurableFunctions/ConfigurationFile/RuntimeConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyncAzureDurableFunctions.ConfigurationFile
+{
+    public static class RuntimeConfig
+    {
+        public static AppSettings appsettings { get; set; }
+    }
+}
